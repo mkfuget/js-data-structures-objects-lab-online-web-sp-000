c80665dfc630d467ad7b9df475c163fe13969ee3
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 08:06:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 function updateDriverWithKeyAndValue(driver, key, value)
 {
-  let hash = {
+  const hash = {
     key: value
   }
   out = Object.assign(driver, hash)
